f4f9b77ca4c76b377ce222a2a5ad88bb166cf56a
Erman Coskun <115878270+ermancoskun@users.noreply.github.com>
2023-04-18 14:03:18 +0300

    v2

---
+++ src/test/java/ArabamAppTest.java
@@ -0,0 +1,70 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.aspectj.lang.annotation.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ArabamAppTest {
+
+    AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
+
+    final String cihazAdi="PIXEL";
+    final String platformIsmi="Android";
+    final String version="10.0";
+    final String automation="UiAutomator2";
+
+@BeforeTest
+    public void setUp() throws MalformedURLException {
+
+    DesiredCapabilities capabilities= new DesiredCapabilities();
+
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazAdi);
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+    capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
+    capabilities.setCapability("appPackage","com.dogan.arabam"); // hangi uygulama uzerinde calismak istedigimizi apk infodan
+    capabilities.setCapability("appActivity","com.dogan.arabam.presentation.feature.home.HomeActivity"); // uygulamada hangi sayfayi gormek veya baslatmak istedgimiz
+    //genelde Main.Activity ya da Homepage.activity olarak apk info icersinde activities kisminda gorulur
+    capabilities.setCapability(MobileCapabilityType.NO_RESET,false);
+    // eger false kullanirsak uygulama calistiktan sonra yapilacak adimlari gerceklestirir uygulamayi islem bittikten sonra SIFIRLAR
+    // eger true olursa uygulama calistiktan sonra yapilacak adimlari gercceklestirir uygulamayi islem bittikten sonra SIFIRLAMAZ
+    driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+}
+
+    @Test
+    public void arabamTest(){
+        // driver.findElement(By.xpath("//*[@text='İlan ver']")).click();
+        // Arabam kac para bolumune tiklayalim
+        driver.findElement(By.xpath("(//*[@text='Arabam kaç para?'])[1]")).click();
+        // Aracimin fiyatini merak ediyorum bolumunetiklayalim
+        AndroidElement fiyatMerak =driver.findElement(By.xpath("//*[@text='Aracımın fiyatını merak ediyorum']"));
+        fiyatMerak.click();
+        // Wolkswagen markasini secelim
+        // yil secimi yapalim
+        // model secimi yapalim
+        // govde tipini secelim
+        // yakit tipini secelim
+        // vites tipini secelim
+        // Versiyon secimi yapalim
+        // aracin km bilgilerini girelim
+        // aracin rengini secelim
+        // opsiyel donanim (varsa) seecelim
+        // degisen bilgisi ekleyerek tramer kaydi belirtelim
+        // aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
+        // uygulamayi kapatalim
+
+}
+
+
+
+
+}
--- src/test/java/DenemeTest.java
+++ src/test/java/DenemeTest.java
@@ -1,7 +1,10 @@
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -10,25 +13,51 @@ import java.util.concurrent.TimeUnit;
 
 public class DenemeTest {
 
-    AndroidDriver<MobileElement> driver;
+    AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
+    // AppiumDriver<MobileElement> appiumDriver; // hem android hem ios calisir
+
+    final String cihazAdi="PIXEL";
+    final String platformIsmi="Android";
+    final String version="10.0";
+    final String automation="UiAutomator2";
 
     @Test
     public void deneme1() throws MalformedURLException {
 
         DesiredCapabilities capabilities= new DesiredCapabilities();
 
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PIXEL");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Admin\\Downloads\\QA\\APPIUM_T108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazAdi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
+        //capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Admin\\Downloads\\QA\\APPIUM_T108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
 
-        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         // driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
 
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
 
+    @Test
+    public void test01(){
+        driver.activateApp("com.google.android.calculator");
+    driver.findElementByAccessibilityId("8").click();
+    driver.findElementByAccessibilityId("0").click();
+    driver.findElementByAccessibilityId("multiply").click();
+    driver.findElementByAccessibilityId("2").click();
+    driver.findElementByAccessibilityId("0").click();
+    String result=driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        System.out.println("result = " + result);
+
+        Assert.assertEquals(Integer.parseInt(result),1600);
+    }
+
+    @Test
+    public void toplama(){
+
+        driver.activateApp("com.google.android.calculator");
+
+    }
 
 }
