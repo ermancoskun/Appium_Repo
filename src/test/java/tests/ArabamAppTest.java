68d165af0462fc3d40f17b19690dd4bda60f4b7b
Erman Coskun <115878270+ermancoskun@users.noreply.github.com>
2023-04-27 19:45:54 +0300

    v4

--- src/test/java/tests/ArabamAppTest.java
+++ src/test/java/tests/ArabamAppTest.java
@@ -20,6 +20,22 @@ import java.util.concurrent.TimeUnit;
 
 public class ArabamAppTest {
 
+    // Arabam kac para bolumune tiklayalim
+// Aracimin fiyatini merak ediyorum bolumunetiklayalim
+// Wolkswagen markasini secelim
+// yil secimi yapalim
+// model secimi yapalim
+// govde tipini secelim
+// yakit tipini secelim
+// vites tipini secelim
+// Versiyon secimi yapalim
+// aracin km bilgilerini girelim
+// aracin rengini secelim
+// opsiyel donanim (varsa) seecelim
+// degisen bilgisi ekleyerek tramer kaydi belirtelim
+// aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
+// uygulamayi kapatalim
+
     AndroidDriver<AndroidElement> driver; //android cihazlarin driveri
 
     final String cihazAdi="PIXEL2";
